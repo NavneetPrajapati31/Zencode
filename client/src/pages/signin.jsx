74655d5683a155ac1c3b241f558a1afe96bc53e9
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-07-13 21:12:21 +0530

    cleared complete frontend to fix tailwindcss

--- client/src/pages/signin.jsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from "../components/sign-in-form";
-
-const SignInPage = () => (
-  <main className="min-h-screen bg-gray-50 flex items-center justify-center">
-    <SignInForm />
-  </main>
-);
-
-export default SignInPage;
--- client/src/pages/signup.jsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "../components/sign-up-form";
-
-const SignUpPage = () => (
-  <main className="min-h-screen bg-gray-50 flex items-center justify-center">
-    <SignUpForm />
-  </main>
-);
-
-export default SignUpPage;
