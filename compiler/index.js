0a76a2606a85f05ef2119b7effb28ab05c3badc5
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-07-14 22:48:37 +0530

    cors added for support

--- compiler/index.js
+++ compiler/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const generateFile = require("./generateFile");
 const executeCpp = require("./executeCpp");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
